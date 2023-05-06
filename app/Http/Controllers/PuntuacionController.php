c16d8260a24650891da081d1d061f93de762ade6
MAC <mac@MacBook-Pro-de-MacBook.local>
2023-05-06 11:22:00 -0400

    se actualiza el control para ver mas datos

--- app/Http/Controllers/PuntuacionController.php
+++ app/Http/Controllers/PuntuacionController.php
@@ -52,12 +52,12 @@ class PuntuacionController extends Controller
     //vamos a listar y saber si fue atenda por el stylis
 
     public function showStylistsAndServices($name)
-{
-    $stylistsAndServices = Client::select('stylist', 'service')
-        ->where('name', $name)
-        ->groupBy('stylist', 'service')
-        ->get();
+    {
+        $stylistsAndServices = Client::select('stylist', 'service','stylistName','name')
+            ->where('name', $name)
+            ->groupBy('stylist', 'service','stylistName','name')
+            ->get();
 
-    return response()->json($stylistsAndServices);
-}
+        return response()->json($stylistsAndServices);
+    }
 }
